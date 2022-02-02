36b9b8e610b8b8d376652fddaeca2754dfae5052
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2022-02-02 11:55:38 -0600

    update

--- src/pages/new.js
+++ src/pages/new.js
@@ -1,6 +1,7 @@
 import Layout from '../components/Layout'
 import {useState} from 'react'
-import {useTask} from '../context/taskContext'
+import {useTask} from '../context/taskContext';
+
 const TaskFormPage = () => {
     const [task ,setTask]= useState({
         title:'',
@@ -12,10 +13,11 @@ const TaskFormPage = () => {
     const handleChange = (e)=>{
         const {name , value} = e.target
        setTask({...task ,[name]:value})
+       
     }
     const handleSubmit = (e)=>{
       e.preventDefault();
-     createTask(task)
+      createTask(task)
     }
   return (
     <Layout>
@@ -37,8 +39,8 @@ const TaskFormPage = () => {
       focus:outline-none
       w-full
       py-3 px-4 mb-5"
-      name='dec'
-      onChange={handleChange}></textarea>
+      name = 'dec'
+      onChange = {handleChange}></textarea>
       <button className="bg-green-500 hover:bg-green-400 py-2 px-4 rounded-sm disabled:opacity-30 "
        disabled ={!task.title}>Save</button>
  </form>
