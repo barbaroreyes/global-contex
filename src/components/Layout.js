bf2ad9c388223fcc04a1216e72b6c1772f1ed257
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2022-02-01 20:30:02 -0600

    added-icons

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,15 +2,18 @@ import React from 'react';
 
 const Layout = ({children}) => {
   return (
-    <div>
+    <div className = ' h-screen bg-gray-900 text-white'>
      <header className="bg-gray-800 text-white flex px-20 py-5">
      <h1 className="font-black text-lg">Task App</h1>
       <div className="flex-grow text-right">
-          <button className="bg-green-300 px-3 py-2 text-black">Add Task</button>
+          <button className="bg-green-500 hover:bg-green-300 px-5 py-2 font-bold rouded-sm">Add Task</button>
       </div>
      </header>
-     <hr/>
+   
+     <main className="px-28">
      {children}
+     </main>
+     
     </div>
   );
 }
